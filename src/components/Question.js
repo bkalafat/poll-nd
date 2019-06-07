eccf7c6861331fbe952c89673273eba577799e49
bkalafat <kalafatburak@gmail.com>
2019-06-07 18:36:08 +0300

    feat: Add question answer view and save answers.

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,32 +1,47 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom'
+import { saveQuestionAnswer } from '../utils/api';
 
 class Question extends Component {
+
+  onOptionClicked = (e) => {
+    e.preventDefault();
+    const { authedUser, question } = this.props
+    const id = question.id
+    const answer = e.target.value
+    saveQuestionAnswer({authedUser,id, answer})
+  }
+
+  onOptionTwoClicked = (e) => {
+    e.preventDefault();
+    //TODO bkalafat: Add question optionTwo votes array authedUser.
+  }
+
   render() {
-    const {question, author} = this.props
+    const { question, author } = this.props
 
-
-    if(!question) {
+    if (!question) {
       return <p>Loading</p>
     }
 
-    return(
-    <div>
-      <div>{author.name} asked:</div>
-      <div>{question.optionOne.text}</div>
-      <div>{question.optionTwo.text}</div>
-    </div>
+    return (
+      <div id="center">
+        <h5>{author.name} asked:</h5>
+        <button type="button" value="optionOne" onClick={this.onOptionClicked} className="btn btn-outline-primary btn-lg btn-block">{question.optionOne.text}</button>
+        <button type="button" value="optionTwo" onClick={this.onOptionClicked} className="btn btn-outline-primary btn-lg btn-block">{question.optionTwo.text}</button>
+      </div>
     )
   }
 }
 
-function mapStateToProps({questions, users}, props) {
-  const {id} = props.match.params
+function mapStateToProps({ authedUser, questions, users }, props) {
+  const { id } = props.match.params
   const question = questions[id]
-  if(question) {
+  if (question) {
     const author = users[question.author]
     return {
+      authedUser,
       question,
       author
     }
--- src/components/QuestionCard.js
+++ src/components/QuestionCard.js
@@ -1,23 +1,22 @@
-import React, {Component}  from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 class QuestionCard extends Component {
 
-  render () {
+  render() {
     const { user, question } = this.props;
 
     return (
-    <div className="jumbotron" >
-      <h5><img className="img-thumbnail col-8 col-sm-8 col-md-6 col-lg-4" src={user.avatarURL} alt={`Avatar of ${user.name}`}/> {user.name} asks:</h5>
-      <h5>Would you rather</h5>
-      <p >{question.optionOne.text}</p>
-      <a href={`/question/${question.id}`} className="btn btn-primary btn-sm" role="button">View Poll</a>
-    </div>)
+      <div className="jumbotron" >
+        <h5><img className="img-thumbnail col-8 col-sm-8 col-md-6 col-lg-4" src={user.avatarURL} alt={`Avatar of ${user.name}`} /> {user.name} asks:</h5>
+        <h5>Would you rather</h5>
+        <p >{question.optionOne.text}</p>
+        <a href={`/question/${question.id}`} className="btn btn-primary btn-sm" role="button">View Poll</a>
+      </div>)
   }
 }
 
-function mapStateToProps({authedUser, users,questions} ,{id})
-{
+function mapStateToProps({ authedUser, users, questions }, { id }) {
   const question = questions[id]
   const user = users[questions[id].author]
   return {
